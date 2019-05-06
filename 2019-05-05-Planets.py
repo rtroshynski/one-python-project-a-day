a7fbb8da8e46f9e3db4715c0d8fa227461fdded2
Robert Troshynski <rtroshynski@hotmail.com>
2019-05-05 22:44:38 -0500

    Create 2019-05-05-Planets.py

---
+++ 2019-05-05-Planets.py
@@ -0,0 +1,6 @@
+planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+print(planets[0:0]) # [] empty set 
+print(planets[0:1]) # ['Mercury']
+print(planets[0:])  # ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+print(planets[:7])  # ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus']
+#eof
