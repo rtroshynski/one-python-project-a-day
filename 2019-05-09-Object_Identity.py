a05f229312e00acc2ab0ccd7faa3a715b524ebee
Robert Troshynski <rtroshynski@hotmail.com>
2019-05-09 23:30:42 -0500

    Create 2019-05-09-Object_Identity.py

---
+++ 2019-05-09-Object_Identity.py
@@ -0,0 +1,17 @@
+m = 100
+print( id(m) )
+n = 100
+print( id(n) )
+a = "foo"
+print( id(a) )
+b = a
+print( id(b) )
+x = 500
+y = 500
+print( id(x) )
+print( id(y) )
+t = 5
+u = 5
+print( id(t) )
+print( id(u) )
+#eof
