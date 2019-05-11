5cbc7a002915feeb08b28138aedd0cf3d7b22d0d
Robert Troshynski <rtroshynski@hotmail.com>
2019-05-10 23:37:56 -0500

    Create 2019-05-10-Equality_Comparison.py

---
+++ 2019-05-10-Equality_Comparison.py
@@ -0,0 +1,11 @@
+a = 1
+b = 2
+print(+a)
+print(-b)
+# equality comparison on floating point values
+x = 1.1 + 2.2
+print (x == 3.3)
+# to determine equality use a tolerance value
+tolerance = 0.00001
+print( abs(x - 3.3) < tolerance )
+#eof
