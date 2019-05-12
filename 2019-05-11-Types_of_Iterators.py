8f94b128d36116d2acbbe89e1339cd8e363550fe
Robert Troshynski <rtroshynski@hotmail.com>
2019-05-11 23:43:13 -0500

    Create 2019-05-11-Types_of_Iterators.py

---
+++ 2019-05-11-Types_of_Iterators.py
@@ -0,0 +1,6 @@
+print(iter('foobar'))                       # string iterator
+print(iter(['foo', 'bar', 'baz']))          # list iterator
+print(iter(('foo', 'bar', 'baz')))          # tuple iterator
+print(iter({'foo', 'bar', 'baz'}))          # set iterator
+print(iter({'foo': 1, 'bar': 2, 'baz': 3})) # dict_key iterator
+#eof
