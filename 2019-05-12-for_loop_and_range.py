9498b74a26d1f29a8f66dd54a5921bb908fcf29f
Robert Troshynski <rtroshynski@hotmail.com>
2019-05-12 23:20:57 -0500

    Create 2019-05-12-for_loop_and_range.py

---
+++ 2019-05-12-for_loop_and_range.py
@@ -0,0 +1,10 @@
+# prints 1-8
+for n in (1,2,3,4,5,6,7,8):
+	print(n)
+# prints 1-7
+x = range(1,8)
+for n in x:
+	print(n)
+# prints 5, 8, 11, 14, 17 as a list
+print( list(range(5, 20, 3)) )
+#eof
