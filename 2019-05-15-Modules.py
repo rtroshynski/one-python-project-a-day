bc7f295d102cf344a5a66129a7d83ac1004acb15
Robert Troshynski <rtroshynski@hotmail.com>
2019-05-15 21:45:20 -0500

    Create 2019-05-15-Modules.py

---
+++ 2019-05-15-Modules.py
@@ -0,0 +1,12 @@
+import math
+print(math.sqrt(16))
+#
+from math import ceil, floor
+print(ceil(3.7))
+print(floor(3.7))
+#
+import math as m
+print(math.sqrt(16) == m.sqrt(16))
+#
+print(dir(math))
+#eof
