3d34288830d36f5362b41fa9c1c4b23c42253340
Robert Troshynski <rtroshynski@hotmail.com>
2019-05-16 21:38:51 -0500

    Create 2019-05-16-Functions.py

---
+++ 2019-05-16-Functions.py
@@ -0,0 +1,9 @@
+def add(a, b):
+    return a + b
+print(add(3,4))
+print(add(b=5,a=6))
+def mixed_function(a, b=1, c=3):
+	return a + b + c
+print(mixed_function(1))
+print(mixed_function(2,b=7,c=9))
+#eof
