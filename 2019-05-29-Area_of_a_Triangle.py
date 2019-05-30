267d9d239d78f4a89f460088bcea496c4b5f8695
Robert Troshynski <rtroshynski@hotmail.com>
2019-05-29 23:32:44 -0500

    Create 2019-05-29-Area_of_a_Triangle.py

---
+++ 2019-05-29-Area_of_a_Triangle.py
@@ -0,0 +1,14 @@
+# https://www.programiz.com/python-programming/examples/area-triangle
+# Program to find the area of triangle using Heron's formula
+#
+a = 5
+b = 6
+c = 7
+#
+# calculate the semi-perimeter
+s = (a + b + c) / 2
+#
+# calculate the area
+area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
+print('The area of the triangle is %0.2f' %area)
+#eof
