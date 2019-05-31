1e6448e0d75ba8da28226d26a02d556badc8baee
Robert Troshynski <rtroshynski@hotmail.com>
2019-05-30 22:54:43 -0500

    Update 2019-05-30-Area_of_a_Circle.py

--- 2019-05-30-Area_of_a_Circle.py
+++ 2019-05-30-Area_of_a_Circle.py
@@ -3,14 +3,14 @@ import math
 def calculateArea(radius):
     area = (math.pi * (radius**2))
     return area
-
+#
 n = int(input("Please enter the amount of circles you wish to test: "))
 print(n)
 print("")
 print("{:10} {:15} {:15}".format("Circle('s)","Radius Given","Calculated Area") )
 print("{:10} {:15} {:15}".format("-"*10,"-"*15,"-"*15) )
-
-
+#
+#
 for x in range(1, n + 1):
     print("{:10} {:15} {:15}".format(x, x, calculateArea(x)) )
 #eof
