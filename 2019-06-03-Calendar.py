9a0b73b84aef8afbdb991c0df9077ce841d5299c
Robert Troshynski <rtroshynski@hotmail.com>
2019-06-03 23:48:04 -0500

    Create 2019-06-03-Calendar.py

---
+++ 2019-06-03-Calendar.py
@@ -0,0 +1,9 @@
+# https://www.w3resource.com/python-exercises/python-basic-exercise-12.php
+import calendar
+y = int(input("Input the year : "))
+print(y)
+m = int(input("Input the month : "))
+print(m)
+print("")
+print(calendar.month(y, m))
+#eof
