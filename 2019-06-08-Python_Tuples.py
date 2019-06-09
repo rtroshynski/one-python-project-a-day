e56e04b220052a5f2a4144008da8e4923680c93c
Robert Troshynski <rtroshynski@hotmail.com>
2019-06-08 23:41:49 -0500

    Create 2019-06-08-Python_Tuples.py

---
+++ 2019-06-08-Python_Tuples.py
@@ -0,0 +1,28 @@
+# https://learnxinyminutes.com/docs/python3/
+# Tuples are like lists but are immutable.
+tup = (1, 2, 3)
+tup[0]      # => 1
+#tup[0] = 3  # Raises a TypeError
+
+# Note that a tuple of length one has to have a comma after the last element but
+# tuples of other lengths, even zero, do not.
+type((1))   # => <class 'int'>
+type((1,))  # => <class 'tuple'>
+type(())    # => <class 'tuple'>
+
+# You can do most of the list operations on tuples too
+len(tup)         # => 3
+tup + (4, 5, 6)  # => (1, 2, 3, 4, 5, 6)
+tup[:2]          # => (1, 2)
+2 in tup         # => True
+
+# You can unpack tuples (or lists) into variables
+a, b, c = (1, 2, 3)  # a is now 1, b is now 2 and c is now 3
+# You can also do extended unpacking
+a, *b, c = (1, 2, 3, 4)  # a is now 1, b is now [2, 3] and c is now 4
+# Tuples are created by default if you leave out the parentheses
+d, e, f = 4, 5, 6  # tuple 4, 5, 6 is unpacked into variables d, e and f
+# respectively such that d = 4, e = 5 and f = 6
+# Now look how easy it is to swap two values
+e, d = d, e  # d is now 5 and e is now 4
+# eof
