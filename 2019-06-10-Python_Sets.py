82ee5310517b4c2e82f6e77080c44ab9707745f7
Robert Troshynski <rtroshynski@hotmail.com>
2019-06-10 23:12:32 -0500

    Create 2019-06-10-Python_Sets.py

---
+++ 2019-06-10-Python_Sets.py
@@ -0,0 +1,41 @@
+# https://learnxinyminutes.com/docs/python3/
+# Sets store ... well sets
+empty_set = set()
+
+# Initialize a set with a bunch of values. Yeah, it looks a bit like a dict.
+some_set = {1, 1, 2, 2, 3, 4}  # some_set is now {1, 2, 3, 4}
+
+# Similar to keys of a dictionary, elements of a set have to be immutable.
+#invalid_set = {[1], 1}  # => Raises a TypeError: unhashable type: 'list'
+valid_set = {(1,), 1}
+
+# Add one more item to the set
+filled_set = some_set
+filled_set.add(5)  # filled_set is now {1, 2, 3, 4, 5}
+
+# Sets do not have duplicate elements
+filled_set.add(5)  # it remains as before {1, 2, 3, 4, 5}
+
+# Do set intersection with &
+other_set = {3, 4, 5, 6}
+filled_set & other_set  # => {3, 4, 5}
+
+# Do set union with |
+filled_set | other_set  # => {1, 2, 3, 4, 5, 6}
+
+# Do set difference with -
+{1, 2, 3, 4} - {2, 3, 5}  # => {1, 4}
+
+# Do set symmetric difference with ^
+{1, 2, 3, 4} ^ {2, 3, 5}  # => {1, 4, 5}
+
+# Check if set on the left is a superset of set on the right
+{1, 2} >= {1, 2, 3} # => False
+
+# Check if set on the left is a subset of set on the right
+{1, 2} <= {1, 2, 3} # => True
+
+# Check for existence in a set with in
+2 in filled_set   # => True
+10 in filled_set  # => False
+# eof
