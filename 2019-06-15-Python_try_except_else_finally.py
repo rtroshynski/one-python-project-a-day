1a880626a33701a6d3a3371c3636af56b9fb43eb
Robert Troshynski <rtroshynski@hotmail.com>
2019-06-15 23:40:42 -0500

    Create 2019-06-15-Python_try_except_else_finally.py

---
+++ 2019-06-15-Python_try_except_else_finally.py
@@ -0,0 +1,10 @@
+# chapter 9: Python For Programmers (Deitel)
+try:
+	print('try suite with no raised exception')
+except:
+	print('this will not execute')
+else:
+	print('else executes because no exceptions in the try suite')
+finally:
+	print('finally always executes')
+# eof
