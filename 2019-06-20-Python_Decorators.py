ff41920ddbf1ed1e70601c271a8b55338db10cc1
Robert Troshynski <rtroshynski@hotmail.com>
2019-06-20 23:39:07 -0500

    Create 2019-06-20-Python_Decorators.py

---
+++ 2019-06-20-Python_Decorators.py
@@ -0,0 +1,292 @@
+# Python 3.7
+# https://book.pythontips.com/en/latest/decorators.html
+# understanding functions
+def hi(name="yasoob"):
+    return "hi " + name
+
+print(hi())
+# output: 'hi yasoob'
+
+# We can even assign a function to a variable like
+greet = hi
+# We are not using parentheses here because we are not calling the function hi
+# instead we are just putting it into the greet variable. Let's try to run this
+
+print(greet())
+# output: 'hi yasoob'
+
+# Let's see what happens if we delete the old hi function!
+#del hi
+#print(hi())
+#outputs: NameError
+
+print(greet())
+#outputs: 'hi yasoob'
+#
+#
+# defining functions inside other functions
+def hi(name="yasoob"):
+    print("now you are inside the hi() function")
+
+    def greet():
+        return "now you are in the greet() function"
+
+    def welcome():
+        return "now you are in the welcome() function"
+
+    print(greet())
+    print(welcome())
+    print("now you are back in the hi() function")
+
+hi()
+#output:now you are inside the hi() function
+#       now you are in the greet() function
+#       now you are in the welcome() function
+#       now you are back in the hi() function
+
+# This shows that whenever you call hi(), greet() and welcome()
+# are also called. However the greet() and welcome() functions
+# are not available outside the hi() function e.g:
+
+#greet()
+#outputs: NameError: name 'greet' is not defined
+#
+#
+# returning functions from other functions
+def hi(name="yasoob"):
+    def greet():
+        return "now you are in the greet() function"
+
+    def welcome():
+        return "now you are in the welcome() function"
+
+    if name == "yasoob":
+        return greet
+    else:
+        return welcome
+
+a = hi()
+print(a)
+#outputs: <function greet at 0x7f2143c01500>
+
+#This clearly shows that `a` now points to the greet() function in hi()
+#Now try this
+
+print(a())
+#outputs: now you are in the greet() function
+#
+# giving a function as an argument to another function
+def hi():
+    return "hi yasoob!"
+
+def doSomethingBeforeHi(func):
+    print("I am doing some boring work before executing hi()")
+    print(func())
+
+doSomethingBeforeHi(hi)
+#outputs:I am doing some boring work before executing hi()
+#        hi yasoob!
+#
+#
+# writing your first decorator
+def a_new_decorator(a_func):
+
+    def wrapTheFunction():
+        print("I am doing some boring work before executing a_func()")
+
+        a_func()
+
+        print("I am doing some boring work after executing a_func()")
+
+    return wrapTheFunction
+
+def a_function_requiring_decoration():
+    print("I am the function which needs some decoration to remove my foul smell")
+
+a_function_requiring_decoration()
+#outputs: "I am the function which needs some decoration to remove my foul smell"
+
+a_function_requiring_decoration = a_new_decorator(a_function_requiring_decoration)
+#now a_function_requiring_decoration is wrapped by wrapTheFunction()
+
+a_function_requiring_decoration()
+#outputs:I am doing some boring work before executing a_func()
+#        I am the function which needs some decoration to remove my foul smell
+#        I am doing some boring work after executing a_func()
+#
+@a_new_decorator
+def a_function_requiring_decoration():
+    """Hey you! Decorate me!"""
+    print("I am the function which needs some decoration to "
+          "remove my foul smell")
+
+a_function_requiring_decoration()
+#outputs: I am doing some boring work before executing a_func()
+#         I am the function which needs some decoration to remove my foul smell
+#         I am doing some boring work after executing a_func()
+
+#the @a_new_decorator is just a short way of saying:
+a_function_requiring_decoration = a_new_decorator(a_function_requiring_decoration)
+#
+#
+from functools import wraps
+
+def a_new_decorator(a_func):
+    @wraps(a_func)
+    def wrapTheFunction():
+        print("I am doing some boring work before executing a_func()")
+        a_func()
+        print("I am doing some boring work after executing a_func()")
+    return wrapTheFunction
+
+@a_new_decorator
+def a_function_requiring_decoration():
+    """Hey yo! Decorate me!"""
+    print("I am the function which needs some decoration to "
+          "remove my foul smell")
+
+print(a_function_requiring_decoration.__name__)
+# Output: a_function_requiring_decoration
+#
+#
+from functools import wraps
+def decorator_name(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if not can_run:
+            return "Function will not run"
+        return f(*args, **kwargs)
+    return decorated
+
+@decorator_name
+def func():
+    return("Function is running")
+
+can_run = True
+print(func())
+# Output: Function is running
+
+can_run = False
+print(func())
+# Output: Function will not run
+#
+#
+# authorization
+from functools import wraps
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            authenticate()
+        return f(*args, **kwargs)
+    return decorated
+#
+#
+# logging 
+from functools import wraps
+
+def logit(func):
+    @wraps(func)
+    def with_logging(*args, **kwargs):
+        print(func.__name__ + " was called")
+        return func(*args, **kwargs)
+    return with_logging
+
+@logit
+def addition_func(x):
+   """Do some math."""
+   return x + x
+
+
+result = addition_func(4)
+# Output: addition_func was called
+#
+#
+# nesting a decorator within a function
+from functools import wraps
+
+def logit(logfile='out.log'):
+    def logging_decorator(func):
+        @wraps(func)
+        def wrapped_function(*args, **kwargs):
+            log_string = func.__name__ + " was called"
+            print(log_string)
+            # Open the logfile and append
+            with open(logfile, 'a') as opened_file:
+                # Now we log to the specified logfile
+                opened_file.write(log_string + '\n')
+        return wrapped_function
+    return logging_decorator
+
+@logit()
+def myfunc1():
+    pass
+
+myfunc1()
+# Output: myfunc1 was called
+# A file called out.log now exists, with the above string
+
+@logit(logfile='func2.log')
+def myfunc2():
+    pass
+
+myfunc2()
+# Output: myfunc2 was called
+# A file called func2.log now exists, with the above string
+#
+#
+# decorator classes
+class logit(object):
+
+    _logfile = 'out.log'
+
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, *args):
+        log_string = self.func.__name__ + " was called"
+        print(log_string)
+        # Open the logfile and append
+        with open(self._logfile, 'a') as opened_file:
+            # Now we log to the specified logfile
+            opened_file.write(log_string + '\n')
+        # Now, send a notification
+        self.notify()
+
+        # return base func
+        return self.func(*args)
+
+
+
+    def notify(self):
+        # logit only logs, no more
+        pass
+#
+#
+# another implementation
+logit._logfile = 'out2.log' # if change log file
+@logit
+def myfunc1():
+    pass
+
+myfunc1()
+# Output: myfunc1 was called
+#
+#
+# subclassing logit to add email functionality
+class email_logit(logit):
+    '''
+    A logit implementation for sending emails to admins
+    when the function is called.
+    '''
+    def __init__(self, email='admin@myproject.com', *args, **kwargs):
+        self.email = email
+        super(email_logit, self).__init__(*args, **kwargs)
+
+    def notify(self):
+        # Send an email to self.email
+        # Will not be implemented here
+        pass
+# eof
