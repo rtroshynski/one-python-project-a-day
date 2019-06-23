a4dc233d3e43b80f1a04d03effe25d507861de35
Robert Troshynski <rtroshynski@hotmail.com>
2019-06-22 23:53:56 -0500

    Create 2019-06-22-Python_defaultdict.py

---
+++ 2019-06-22-Python_defaultdict.py
@@ -0,0 +1,33 @@
+# Python 3.7
+https://book.pythontips.com/en/latest/collections.html
+#
+from collections import defaultdict
+
+colours = (
+    ('Yasoob', 'Yellow'),
+    ('Ali', 'Blue'),
+    ('Arham', 'Green'),
+    ('Ali', 'Black'),
+    ('Yasoob', 'Red'),
+    ('Ahmed', 'Silver'),
+)
+
+favourite_colours = defaultdict(list)
+
+for name, colour in colours:
+    favourite_colours[name].append(colour)
+
+print(favourite_colours)
+
+# output
+# defaultdict(<type 'list'>,
+#    {'Arham': ['Green'],
+#     'Yasoob': ['Yellow', 'Red'],
+#     'Ahmed': ['Silver'],
+#     'Ali': ['Blue', 'Black']
+# })
+
+import json
+print(json.dumps(some_dict))
+# Output: {"colours": {"favourite": "yellow"}}
+# eof
