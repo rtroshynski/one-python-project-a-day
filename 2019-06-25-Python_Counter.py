660cfc472b82c4f0f75c3bee45a5953d8e30db32
Robert Troshynski <rtroshynski@hotmail.com>
2019-06-25 23:36:44 -0500

    Create 2019-06-25-Python_Counter.py

---
+++ 2019-06-25-Python_Counter.py
@@ -0,0 +1,22 @@
+# Python 3.7
+# https://book.pythontips.com/en/latest/collections.html
+from collections import Counter
+
+colours = (
+    ('Yasoob', 'Yellow'),
+    ('Ali', 'Blue'),
+    ('Arham', 'Green'),
+    ('Ali', 'Black'),
+    ('Yasoob', 'Red'),
+    ('Ahmed', 'Silver'),
+)
+
+favs = Counter(name for name, colour in colours)
+print(favs)
+# Output: Counter({
+#    'Yasoob': 2,
+#    'Ali': 2,
+#    'Arham': 1,
+#    'Ahmed': 1
+# })
+#eof
