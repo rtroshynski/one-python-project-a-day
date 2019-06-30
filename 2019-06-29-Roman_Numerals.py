bd288d654e9d212f13baa9a089bbc4912e21a8a3
Robert Troshynski <rtroshynski@hotmail.com>
2019-06-29 23:07:22 -0500

    Create 2019-06-29-Roman_Numerals.py

---
+++ 2019-06-29-Roman_Numerals.py
@@ -0,0 +1,9 @@
+# Python 3.7
+# if on a windows system, download roman source code from
+# /Python-3.4.1/Doc/tools/roman.py
+import roman
+n=roman.fromRoman("X")
+print(n)
+n = roman.toRoman(100)
+print(n)
+# eof
