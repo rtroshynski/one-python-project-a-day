cd4c5bd1e419f39cd8dd0bb90da89e2ac30d6075
Robert Troshynski <rtroshynski@hotmail.com>
2019-08-05 06:22:33 -0500

    Create 2019-08-05-Python_Enum.py

---
+++ 2019-08-05-Python_Enum.py
@@ -0,0 +1,264 @@
+# Python 3.7.4
+# https://pymotw.com/3/enum/index.html
+# Tested on https://repl.it/
+# Enum creation
+
+import enum
+
+class BugStatus(enum.Enum):
+
+    new = 7
+    incomplete = 6
+    invalid = 5
+    wont_fix = 4
+    in_progress = 3
+    fix_committed = 2
+    fix_released = 1
+
+
+print('\nMember name: {}'.format(BugStatus.wont_fix.name))
+print('Member value: {}'.format(BugStatus.wont_fix.value)) 
+
+#-------------------------------------------------------------------------
+
+# Enum Iteration
+
+import enum
+
+
+class BugStatus(enum.Enum):
+
+    new = 7
+    incomplete = 6
+    invalid = 5
+    wont_fix = 4
+    in_progress = 3
+    fix_committed = 2
+    fix_released = 1
+
+
+for status in BugStatus:
+    print('{:15} = {}'.format(status.name, status.value))
+	
+#------------------------------------------------------------------------
+
+# Comparing enums 
+
+import enum
+
+
+class BugStatus(enum.Enum):
+
+    new = 7
+    incomplete = 6
+    invalid = 5
+    wont_fix = 4
+    in_progress = 3
+    fix_committed = 2
+    fix_released = 1
+
+
+actual_state = BugStatus.wont_fix
+desired_state = BugStatus.fix_released
+
+print('Equality:',
+      actual_state == desired_state,
+      actual_state == BugStatus.wont_fix)
+print('Identity:',
+      actual_state is desired_state,
+      actual_state is BugStatus.wont_fix)
+print('Ordered by value:')
+try:
+    print('\n'.join('  ' + s.name for s in sorted(BugStatus)))
+except TypeError as err:
+    print('  Cannot sort: {}'.format(err))
+	
+class BugStatus(enum.IntEnum):
+
+    new = 7
+    incomplete = 6
+    invalid = 5
+    wont_fix = 4
+    in_progress = 3
+    fix_committed = 2
+    fix_released = 1
+
+
+print('Ordered by value:')
+print('\n'.join('  ' + s.name for s in sorted(BugStatus)))
+
+#----------------------------------------------------------------------
+
+# Unique Enumeration Values
+
+import enum
+
+
+class BugStatus(enum.Enum):
+
+    new = 7
+    incomplete = 6
+    invalid = 5
+    wont_fix = 4
+    in_progress = 3
+    fix_committed = 2
+    fix_released = 1
+
+    by_design = 4
+    closed = 1
+
+
+for status in BugStatus:
+    print('{:15} = {}'.format(status.name, status.value))
+
+print('\nSame: by_design is wont_fix: ',
+      BugStatus.by_design is BugStatus.wont_fix)
+print('Same: closed is fix_released: ',
+      BugStatus.closed is BugStatus.fix_released)
+	  
+	  
+@enum.unique
+class BugStatus(enum.Enum):
+
+    new = 7
+    incomplete = 6
+    invalid = 5
+    wont_fix = 4
+    in_progress = 3
+    fix_committed = 2
+    fix_released = 1
+
+    # This will trigger an error with unique applied.
+    by_design = 4
+    closed = 1
+	
+#----------------------------------------------------------------------
+
+# Creating Enumerations Programmatically
+
+import enum
+
+
+BugStatus = enum.Enum(
+    value='BugStatus',
+    names=('fix_released fix_committed in_progress '
+           'wont_fix invalid incomplete new'),
+)
+
+print('Member: {}'.format(BugStatus.new))
+
+print('\nAll members:')
+for status in BugStatus:
+    print('{:15} = {}'.format(status.name, status.value))
+	
+	
+import enum
+
+
+BugStatus = enum.Enum(
+    value='BugStatus',
+    names=[
+        ('new', 7),
+        ('incomplete', 6),
+        ('invalid', 5),
+        ('wont_fix', 4),
+        ('in_progress', 3),
+        ('fix_committed', 2),
+        ('fix_released', 1),
+    ],
+)
+
+print('All members:')
+for status in BugStatus:
+    print('{:15} = {}'.format(status.name, status.value))
+	
+#----------------------------------------------------------------------
+
+# Non-integer Member Values
+
+import enum
+
+
+class BugStatus(enum.Enum):
+
+    new = (7, ['incomplete',
+               'invalid',
+               'wont_fix',
+               'in_progress'])
+    incomplete = (6, ['new', 'wont_fix'])
+    invalid = (5, ['new'])
+    wont_fix = (4, ['new'])
+    in_progress = (3, ['new', 'fix_committed'])
+    fix_committed = (2, ['in_progress', 'fix_released'])
+    fix_released = (1, ['new'])
+
+    def __init__(self, num, transitions):
+        self.num = num
+        self.transitions = transitions
+
+    def can_transition(self, new_state):
+        return new_state.name in self.transitions
+
+
+print('Name:', BugStatus.in_progress)
+print('Value:', BugStatus.in_progress.value)
+print('Custom attribute:', BugStatus.in_progress.transitions)
+print('Using attribute:',
+      BugStatus.in_progress.can_transition(BugStatus.new))
+	  
+	  
+# This example expresses the same data as the previous example, 
+# using dictionaries rather than tuples
+
+import enum
+
+
+class BugStatus(enum.Enum):
+
+    new = {
+        'num': 7,
+        'transitions': [
+            'incomplete',
+            'invalid',
+            'wont_fix',
+            'in_progress',
+        ],
+    }
+    incomplete = {
+        'num': 6,
+        'transitions': ['new', 'wont_fix'],
+    }
+    invalid = {
+        'num': 5,
+        'transitions': ['new'],
+    }
+    wont_fix = {
+        'num': 4,
+        'transitions': ['new'],
+    }
+    in_progress = {
+        'num': 3,
+        'transitions': ['new', 'fix_committed'],
+    }
+    fix_committed = {
+        'num': 2,
+        'transitions': ['in_progress', 'fix_released'],
+    }
+    fix_released = {
+        'num': 1,
+        'transitions': ['new'],
+    }
+
+    def __init__(self, vals):
+        self.num = vals['num']
+        self.transitions = vals['transitions']
+
+    def can_transition(self, new_state):
+        return new_state.name in self.transitions
+
+
+print('Name:', BugStatus.in_progress)
+print('Value:', BugStatus.in_progress.value)
+print('Custom attribute:', BugStatus.in_progress.transitions)
+print('Using attribute:',
+      BugStatus.in_progress.can_transition(BugStatus.new))
