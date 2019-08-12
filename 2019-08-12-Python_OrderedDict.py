85f2a320c50542fc2d07ae5d43eea0c725b3158e
Robert Troshynski <rtroshynski@hotmail.com>
2019-08-12 07:23:44 -0500

    Create 2019-08-12-Python_OrderedDict.py

---
+++ 2019-08-12-Python_OrderedDict.py
@@ -0,0 +1,74 @@
+# Python 3.7.4
+# https://pymotw.com/3/collections/ordereddict.html
+# Test on https://repl.it
+
+import collections
+
+print('Regular dictionary:')
+d = {}
+d['a'] = 'A'
+d['b'] = 'B'
+d['c'] = 'C'
+
+for k, v in d.items():
+    print(k, v)
+
+print('\nOrderedDict:')
+d = collections.OrderedDict()
+d['a'] = 'A'
+d['b'] = 'B'
+d['c'] = 'C'
+
+for k, v in d.items():
+    print(k, v)
+
+# equality 
+print('dict       :', end=' ')
+d1 = {}
+d1['a'] = 'A'
+d1['b'] = 'B'
+d1['c'] = 'C'
+
+d2 = {}
+d2['c'] = 'C'
+d2['b'] = 'B'
+d2['a'] = 'A'
+
+print(d1 == d2)
+
+print('OrderedDict:', end=' ')
+
+d1 = collections.OrderedDict()
+d1['a'] = 'A'
+d1['b'] = 'B'
+d1['c'] = 'C'
+
+d2 = collections.OrderedDict()
+d2['c'] = 'C'
+d2['b'] = 'B'
+d2['a'] = 'A'
+
+print(d1 == d2)
+
+# reordering
+
+d = collections.OrderedDict(
+    [('a', 'A'), ('b', 'B'), ('c', 'C')]
+)
+
+print('Before:')
+for k, v in d.items():
+    print(k, v)
+
+d.move_to_end('b')
+
+print('\nmove_to_end():')
+for k, v in d.items():
+    print(k, v)
+
+d.move_to_end('b', last=False)
+
+print('\nmove_to_end(last=False):')
+for k, v in d.items():
+    print(k, v)
+# eof
